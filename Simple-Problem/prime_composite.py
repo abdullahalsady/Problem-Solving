db5d1477e97d67c1cf40d7858163e30f37129c64
Abdullah Al Sady <87411735+abdullahalsady@users.noreply.github.com>
2023-07-01 18:40:12 +0600

    Update prime_composite.py for some reason.

--- Simple-Problem/prime_composite.py
+++ Simple-Problem/prime_composite.py
@@ -1,7 +1,7 @@
 number = 16
 flag = False
 if number == 0 or number == 1:
-    print(f"{number} isn't odd number and not even number.")
+    print(f"{number} isn't prime number and not composite number.")
 elif number > 2:
     for i in range(2, number):
         if number % i == 0:
